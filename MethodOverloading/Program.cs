d5a6b9db15fe1ed83bad3f8a6908dbab2281d51b
Schlabach <matthewshawn89@gmail.com>
2022-12-15 06:20:55 -0500

    initial commit

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,40 @@
 {
     public class Program
     {
+        public static int Add(int num1, int num2)
+         {
+            return num1 + num2;
+         }
+
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+
+        public static string Add(int num1, int num2, bool isTrue)
+        {
+            var sum = num1 + num2;
+
+             if (isTrue == true && sum > 1)
+            {            
+                 return $"{sum} dollars";
+            }
+            else if(isTrue == true && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else if(isTrue == true && sum < 1)
+            {
+                return $"{sum} dollars";
+            }
+            else
+            {
+                return sum.ToString();
+            }
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
         }
     }
 }
